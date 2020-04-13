58fc1001689569855661ca2ecce495eefce84aeb
Vinicius <vinicius-sp@hotmail.com>
2020-04-13 09:10:53 -0300

    fix - Refatorando o código de algumas páginas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,94 +1,66 @@
 import 'package:app_distribuida2/widgets/drawer_list.dart';
 import 'package:app_distribuida2/widgets/app_card_materia_favorito.dart';
 import 'package:app_distribuida2/widgets/app_card_materia.dart';
-//import 'package:app_distribuida2/utils/nav.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-       
-        child: Scaffold(
+      length: 2,
+      child: Scaffold(
         appBar: AppBar(
           //title: Text("Dashboard"),
-            centerTitle: true,
-            
-            actions: <Widget>[
+          centerTitle: true,
+
+          actions: <Widget>[
             //IconButton(icon: Icon(Icons.notifications), onPressed: () {})
-            ],
-            iconTheme: new IconThemeData(color: Colors.white), //cor do menu 
-            backgroundColor: Color(0XFF009086),
-                      
-            bottom: TabBar(
-            
-              unselectedLabelColor: Color(0XFF6EFFE4), //Cor do texto TabBar sem seleção
-              labelColor: const Color(0XFFFFFFFF), //Cor do texto TabBar com seleção
+          ],
+          iconTheme: new IconThemeData(color: Colors.white), //cor do menu
+          backgroundColor: Color(0XFF009086),
+
+          bottom: TabBar(
+              unselectedLabelColor:
+                  Color(0XFF6EFFE4), //Cor do texto TabBar sem seleção
+              labelColor:
+                  const Color(0XFFFFFFFF), //Cor do texto TabBar com seleção
               indicatorColor: Color(0XFFFFFFFF), // Cor da tab selecionada
-              
+
               labelStyle: TextStyle(
                 color: Colors.white,
-                
                 //fontWeight: FontWeight.w200,
               ),
               tabs: [
-              Tab(
-                text: "DISCIPLINAS", 
-                
-              ),
-              Tab(text: "FAVORITOS",),
-            ]),
+                Tab(text: "FAVORITOS"),
+                Tab(text: "DISCIPLINAS"),
+              ]),
         ),
-
         drawer: DrawerList(
             // color: Colors.yellow,
-                
-            ),
-        body: TabBarView(
-          children: [
-          _body(),
-          _bodyFavoritos(
 
-          )
-        ]),
-    ),
+            ),
+        body: TabBarView(children: [_bodyFavoritos(), _body()]),
+      ),
     );
   }
 
+  // Páginas das disciplinas
   _body() {
-               
-        return Container (
-             
-              child: Container(
-                
-                 decoration: BoxDecoration(
-                 //color: Color(0XFFCEFFE4),
-                  color: Colors.lightBlue[50],
-                 //borderRadius: BorderRadius.circular(10)
-                 ),
-                 child: AppCardMaterias(),
-                                
-                 )
- 
-               );
-    
-  } // fim do método _body
-
-  _bodyFavoritos() {
-      return Container (
-             
-              child: Container(
-                 decoration: BoxDecoration(
-                 color: Colors.lightBlue[50],
-                 //borderRadius: BorderRadius.circular(10)
-                 ),
-                 child: AppCardMateriasFavorito(),
-                                
-                 )
- 
-               );
+    return Container(
+        child: Container(
+      decoration: BoxDecoration(
+        color: Colors.lightBlue[50],
+      ),
+      child: AppCardMaterias(),
+    ));
   }
 
-
+  // Página das disciplinas marcadas como favoritas
+  _bodyFavoritos() {
+    return Container(
+        child: Container(
+      decoration: BoxDecoration(color: Colors.lightBlue[50]),
+      child: AppCardMateriasFavorito(),
+    ));
+  }
 }
--- lib/widgets/drawer_list.dart
+++ lib/widgets/drawer_list.dart
@@ -1,56 +1,39 @@
-
 import 'package:flutter/material.dart';
 import 'package:app_distribuida2/utils/nav.dart';
 import 'package:app_distribuida2/pages/login_page.dart';
 
-class  DrawerList extends StatelessWidget {
+class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text("Olá Fulano",),
-            onTap: () {
-              print("Item1");
-              Navigator.pop(context);
-            },
-
-          ),
-           Divider(),
-           ListTile(
-            leading: Icon(Icons.help),
-            title: Text("MINHAS DÚVIDAS",),
-            onTap: () {
-              print("Item2");
-              Navigator.pop(context);
-            },
-
-          ),
-           ListTile(
-            leading: Icon(Icons.people),
-            title: Text("MONITORES",),
-            onTap: () {
-              print("Item3");
-              Navigator.pop(context);
-            },
-
-          ),
-          Divider(),
-            ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text("SAIR",),
-            onTap: () => _onClickLogout(context),
-
-          )
-        ],
-      )
-    );
+        child: ListView(
+      children: <Widget>[
+        _createOption("Olá Fulano", () {Navigator.pop(context);}),
+        Divider(),
+        _createOption("MINHAS DÚVIDAS", () {Navigator.pop(context);}, Icon(Icons.help)),
+        _createOption("MONITORES", () {Navigator.pop(context);}, Icon(Icons.people)),
+        Divider(),
+        _createOption("SAIR", () {_onClickLogout(context);}, Icon(Icons.exit_to_app))
+      ],
+    ));
   }
-}
 
-_onClickLogout(BuildContext context) {
-  Navigator.pop(context);
-  push(context, LoginPage());
+  // Retorna um item do menu formatado
+  ListTile _createOption(text, Function work, [Icon icon]) {
+    return ListTile(
+          leading: icon,
+          title: Text(
+            text,
+          ),
+          onTap: () {
+            work();
+          },
+        );
+  }
+
+  // Desloga o usuário da aplicação
+  _onClickLogout(BuildContext context) {
+    Navigator.pop(context);
+    push(context, LoginPage());
+  }
 }
